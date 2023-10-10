c3b60f087996547ddc38d36381c1802032e888ed
Gustavo Nobrega <gustavo.nobrega@hotmail.com>
2023-10-10 16:57:48 -0300

    initial setup

--- capputeeno/tailwind.config.ts
+++ capputeeno/tailwind.config.ts
@@ -1,17 +1,24 @@
 import type { Config } from 'tailwindcss'
 
 const config: Config = {
-  content: [
-    './src/pages/**/*.{js,ts,jsx,tsx,mdx}',
-    './src/components/**/*.{js,ts,jsx,tsx,mdx}',
-    './src/app/**/*.{js,ts,jsx,tsx,mdx}',
-  ],
+  content: ['./src/**/*.tsx'],
   theme: {
     extend: {
-      backgroundImage: {
-        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
-        'gradient-conic':
-          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
+      maxWidth: {
+        desktop: '1120px',
+      },
+      colors: {
+        'app-red': '#DE3838',
+        'app-orange': '#FD951F',
+        'app-light-orange': '#FFA585',
+        'app-blue': '#115D8C',
+        'app-back-bt': '#617480',
+        'app-green': '#51B853',
+        'app-background-100': '#F0F0F5',
+        'app-background-200': '#E9E9F0',
+        'app-title': '#5D5D6D',
+        'app-text-300': '#737380',
+        'app-text-400': '#41414D',
       },
     },
   },
